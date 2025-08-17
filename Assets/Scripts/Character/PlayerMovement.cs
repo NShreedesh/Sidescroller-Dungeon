224984659379be03652ab31f582a76e6f4c9b38a
Shreedesh <nshreedesh@gmail.com>
2025-08-17 20:51:13 +0545

    accelarate and decerate movespeed

--- Assets/Scripts/Character/PlayerMovement.cs
+++ Assets/Scripts/Character/PlayerMovement.cs
@@ -11,9 +11,21 @@ namespace Dungeon.Character
         [Header("Data")]
         [SerializeField]
         private float moveSpeed = 300;
+        [SerializeField]
+        private float accelarationSpeed = 100;
+        [SerializeField]
+        private float deccelarationSpeed = 50;
+        private float _curentMoveSpeed;
 
         [Header("Inputs")]
         private float _moveInput;
+
+        private void Update()
+        {
+            float deltaValue = Mathf.Sign(_curentMoveSpeed) != Mathf.Sign(_moveInput) ? deccelarationSpeed : accelarationSpeed;
+            if(_moveInput == 0) deltaValue = deccelarationSpeed;
+            _curentMoveSpeed = Mathf.MoveTowards(_curentMoveSpeed, _moveInput * moveSpeed, deltaValue * Time.deltaTime);
+        }
 
         private void FixedUpdate()
         {
@@ -23,7 +35,7 @@ namespace Dungeon.Character
         private void Move()
         {
             Vector3 velocity = rb.linearVelocity;
-            float xMovement = _moveInput * moveSpeed * Time.fixedDeltaTime;
+            float xMovement = _curentMoveSpeed * Time.fixedDeltaTime;
             velocity.x = xMovement;
             rb.linearVelocity = velocity;   
         }
